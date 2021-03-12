8a71f7a5ac9c2894e29a41de2c666462645650d2
Antoine BRIVET <antoine.brivet@le-campus-numerique.fr>
2021-03-12 16:30:47 +0100

    second input

--- applicationvue.js
+++ applicationvue.js
@@ -3,13 +3,12 @@ new Vue({
     el: '#app',
     data: {
       nom: 'Veuillez saisir votre nom',
-      mail: 'Veuillez saisir votre adresse mail'
+      h2: 'Veuillez saisir votre adresse mail',
+      contenu:''
+      
     },
     methods : {
-      validate : function(){
-        this.mail = true;
-         if( this.validEmail(this.email)){
-            this.valid = true;
-         }
-  }}})
-
+      validate:function(){
+        this.h2=this.contenu // 'this.h2' fait référence au 'h2' présent dans l'objet 'data'. 'this.contenu fait référence au 'contenu' present dans l'objet 'data'
+      }
+    }})
