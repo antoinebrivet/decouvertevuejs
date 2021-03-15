3b115d33a765bc5cffcba98ddcf1135377f59b86
antoine brivet <antoine.brivet@le-campus-numerique.fr>
2021-03-15 11:55:15 +0100

    réalisation de l'itération

--- applicationvue.js
+++ applicationvue.js
@@ -4,11 +4,15 @@ new Vue({
     data: {
       nom: 'Veuillez saisir votre nom',
       h2: 'Veuillez saisir votre adresse mail',
-      contenu:''
+      contenu:'',
+      checked: false //Notre valeur 'checked' contenue dans l'objet 'data'.
       
     },
     methods : {
       validate:function(){
         this.h2=this.contenu // 'this.h2' fait référence au 'h2' présent dans l'objet 'data'. 'this.contenu fait référence au 'contenu' present dans l'objet 'data'
-      }
+      },
+      close: function() {
+        this.success = false
+      },
     }})
